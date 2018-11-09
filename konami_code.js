6520668d0d3ef4b5d5c20b1759f32a959fa47ad1
Brooke Gill <brooke.gill25@gmail.com>
2018-11-09 00:20:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+const index = 0;
+
 function init() {
-  // your code here
+  for (let i = 0; i < codes.length - 1; i++) {
+    document.body.addEventListener('keydown', function(event) {
+      const key = event.key;
+
+      if (key === codes[index]) {
+        index++;
+
+        if (index === codes.length) {
+          alert('Hurray!');
+
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    });
+  }
 }
